39a1d137c5cf6c086e2c215f05e316808239f5c3
Pete Jemian <prjemian@gmail.com>
2020-02-20 16:58:52 -0600

    ENH #5 new layout, basic config

---
+++ profile_bluesky/startup/instrument/devices/aps_source.py
@@ -0,0 +1,32 @@
+
+"""
+APS only: connect with facility information
+"""
+
+__all__ = [
+    'aps', 
+    'undulator',
+    ]
+
+from ..session_logs import logger
+logger.info(__file__)
+
+import apstools.devices
+
+from ..framework import sd
+
+class MyApsDevice(apstools.devices.ApsMachineParametersDevice):
+    # for local modifications
+    pass
+
+aps = MyApsDevice(name="aps")
+sd.baseline.append(aps)
+
+# apstools.devices.ApsUndulator
+# apstools.devices.ApsUndulatorDual
+class MyUndulatorDevice(apstools.devices.ApsUndulatorDual):
+    # for local modifications
+    pass
+
+undulator = MyUndulatorDevice("ID29", name="undulator")
+sd.baseline.append(undulator)
